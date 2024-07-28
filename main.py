b377c05e4196fde25538ff7d649a443918268b81
asabase5214 <156596837+asabase5214@users.noreply.github.com>
2024-07-28 19:44:25 +0000

    holidays and the final product

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random, time, sys
+from termcolor import colored
 
 billboards = 0
+achievements = 0
+a1, a2, a3, a4, a5 = False, False, False, False, False
 money = 500000
 TVends = 0
 socialmediaads = 0
@@ -171,6 +174,15 @@ for i in range(365):
   elif i+1 < 366:
     month = 'December'
   print(f'It is {month} {calculateDay(month, i+1)}.')
+  if i+1 == 359:
+    print('Merry Christmas!')
+    time.sleep(3)
+  elif i+1 == 304:
+    print('Happy Halloween!')
+    time.sleep(3)
+  elif i+1 == 89:
+    print('Happy Easter!')
+    time.sleep(3)
   if month in ['December', 'January', 'February']:
     print('It is currently Winter.')
   elif month in ['March', 'April', 'May']:
@@ -181,7 +193,16 @@ for i in range(365):
     print('It is currently Autumn.')
   print('Number of days so far:', i+1)
   money = round(money, 2)
+  original_money = money
   print(f'Money: ${prettyPrintNum(money)}')
+  if money >= 1_000_000_000:
+    if not a5:
+      a5 = True
+      print('\n'*60)
+      print(colored('Congrats! You earned an achievement: \"BILLIONAIRE\"', 'yellow'))
+      print(colored('Have $1,000,000,000 in your bank acount.', 'yellow'))
+      achievements += 1
+      time.sleep(5)
   print(f'Entry price: ${prettyPrintNum(entryPrice)} (per person)')
   print(f'Fast-pass price: ${prettyPrintNum(FastPrice)} (per person)')
   print()
@@ -197,6 +218,12 @@ for i in range(365):
   print('9. Set entry price.')
   print('10. Set FastPass price.')
   print('11. Begin an advertising campaign.')
+  if i+1 == 359:
+    print('12. Put up Christmas decorations!')
+  if i+1 == 304:
+    print('12. Put up Halloween decorations!')
+  if i+1 == 89:
+    print('12. Put up Easter decorations!')
   answer = input('> ')
   if answer == '1':
     print('What type of ride would you like to create?')
@@ -445,6 +472,12 @@ for i in range(365):
             print('You have made', answer, 'billboards.')
             billboards = answer
             Bends = i+29
+            if not a1:
+              a1 = True
+              print('\n'*60)
+              print(colored('Congrats! You earned an achievement: \"Create your first billboard\"', 'yellow'))
+              achievements += 1
+              time.sleep(5)
           else:
             print('You do not have enough money to make this many billboards.')
         else:
@@ -477,8 +510,54 @@ for i in range(365):
           socialmediaads += 1
         else:
           print('You do not have enough money to make this social media ad.')
+  elif answer == '12':
+    if i+1 == 359:
+      print('Your amusement park is now decorated with Christmas trees, lights, snowmen, and more!')
+    elif i+1 == 304:
+      print('Your amusement park is now decorated with pumpkins, ghost, witches, and more!')
+    elif i+1 == 89:
+      print('Your amusement park is now decorated with eggs, bunnies, and more!')
+    time.sleep(5)
+  if original_money - money >= 1000000:
+    if not a3:
+      a3 = True
+      print('\n'*60)
+      print(colored('Congrats! You earned an achievement: \"Budget Buster\"', 'yellow'))
+      print(colored('Spend at least 1 million dollars in a single day.', 'yellow'))
+      achievements += 1
+      time.sleep(5)
   earned, hoolies, guests = simulateGuestsAndIncome(month)
   print(f'You earned ${prettyPrintNum(earned)} today.')
   money += earned
   print(f'{hoolies} people got into your park without paying today. (You got {hoolies} hooligans today)')
+  if hoolies < 100:
+    if not a2:
+      a2 = True
+      print('\n'*60)
+      print(colored('Congrats! You earned an achievement: \"Hooligan Hunter\"', 'yellow'))
+      print(colored('Have only 100 hooligans get into your park one day.', 'yellow'))
+      achievements += 1
+      time.sleep(5)
   print(f'{guests} people bought passes into your park today.')
+  RCs = 0
+  for i in range(len(rides)):
+    if rides[i][0] == 'roller coaster':
+      RCs += 1
+  if RCs >= 10:
+    if not a4:
+      a4 = True
+      print('\n'*60)
+      print(colored('Congrats! You earned an achievement: \"Roller Coaster King\"', 'yellow'))
+      print(colored('Have 10 roller coasters in your park.', 'yellow'))
+      achievements += 1
+      time.sleep(5)
+
+print('Congratulations! You have made it to the end of the year! You have made $'+ str(money - 500000) +'.')
+time.sleep(5)
+print(f'You had {len(rides)} rides.')
+time.sleep(3)
+print(f'You earned {achievements} achievements.')
+time.sleep(3)
+print()
+print('Thank you for playing!')
+sys.exit()
